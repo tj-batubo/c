4bdcc799e9d8c1f355841c23ed7d512725137d98
tj-batubo <tjbatubo@gmail.com>
2025-09-22 23:42:14 +0100

    tried to do it with a pointer

--- cash.c
+++ cash.c
@@ -1,39 +1,64 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_quarters(int change, int *cents);
+int get_dimes(int change, int *cents);
+int get_nickels(int change, int *cents);
+int calculate_change(int change, int *cents);
+
 int main(void)
 {
-
 	int change;
-	do 
+	do
 	{
-		int change = get_int("Change owned: ");
-	} while (change < 1);
-
-	// Calculate how many quarters you should give customer
-    	// Subtract the value of those quarters from cents
-
-    	// Calculate how many dimes you should give customer
-    	// Subtract the value of those dimes from remaining cents
-
-    	// Calculate how many nickels you should give customer
-    	// Subtract the value of those nickels from remaining cents
-
-    	// Calculate how many pennies you should give customer
-    	// Subtract the value of those pennies from remaining cents
-
-    	// Sum the number of quarters, dimes, nickels, and pennies used
-    	// Print that sum
-
-
+		change = get_int("Change owned: ");
+	} while (change < 0);
+	
+	calculate_change(change, &change);
 }
 
-int get_quarters(int change)
+int calculate_change(int change, int *cents)
 {
-	int quarters = change /  0.25;
-	if (quarters % 0.25 != 0)
-	{
-			
-	}
+	int quarters = get_quarters(change, *cents);
+	int dimes = get_dimes(change, *cents);
+	int nickels = get_nickels(change, *cents));
+}
 
+int get_quarters(int change, int *cents)
+{
+	int quarters = 0;
+	while (change >= 25)
+	{
+		quarters++;
+		change -= 25;
+	}
+	
+	*cents = change;
+	return quarters;
+}
+
+int get_dimes(int change, int *cents)
+{
+        int dimes = 0;
+        while (change >= 10)
+        {
+                dimes++;
+                change -= 10;
+        }
+
+        *cents = change;
+        return dimes;
+}
+
+int get_nickels(int change, int *cents)
+{
+        int nickels = 0;
+        while (change >= 5)
+        {
+                nickels++;
+                change -= 5;
+        }
+
+        *cents = change;
+        return nickels;
 }
