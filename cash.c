e02ee1eb1b867bccb18a576a4ea20bd0b4dcc821
tj-batubo <tjbatubo@gmail.com>
2025-09-23 01:05:56 +0100

    cash puzzle solved

--- cash.c
+++ cash.c
@@ -1,64 +1,33 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_quarters(int change, int *cents);
-int get_dimes(int change, int *cents);
-int get_nickels(int change, int *cents);
-int calculate_change(int change, int *cents);
+int calculate_change(int change);
 
 int main(void)
 {
-	int change;
-	do
-	{
-		change = get_int("Change owned: ");
-	} while (change < 0);
-	
-	calculate_change(change, &change);
+    int change;
+    do
+    {
+        change = get_int("Change owned: ");
+    }
+    while (change < 0);
+
+    printf("%i \n", calculate_change(change));
 }
 
-int calculate_change(int change, int *cents)
+int calculate_change(int change)
 {
-	int quarters = get_quarters(change, *cents);
-	int dimes = get_dimes(change, *cents);
-	int nickels = get_nickels(change, *cents));
+    int cents = change / 25;
+    change -= cents * 25;
+
+    int dimes = change / 10;
+    cents += dimes;
+    change -= dimes * 10;
+
+    int nickels = change / 5;
+    cents += nickels;
+    change -= nickels * 5;
+
+    return cents + change;
 }
 
-int get_quarters(int change, int *cents)
-{
-	int quarters = 0;
-	while (change >= 25)
-	{
-		quarters++;
-		change -= 25;
-	}
-	
-	*cents = change;
-	return quarters;
-}
-
-int get_dimes(int change, int *cents)
-{
-        int dimes = 0;
-        while (change >= 10)
-        {
-                dimes++;
-                change -= 10;
-        }
-
-        *cents = change;
-        return dimes;
-}
-
-int get_nickels(int change, int *cents)
-{
-        int nickels = 0;
-        while (change >= 5)
-        {
-                nickels++;
-                change -= 5;
-        }
-
-        *cents = change;
-        return nickels;
-}
