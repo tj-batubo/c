76a0b85d634aa82304dd709694895b6feaefb85e
tj-batubo <tjbatubo@gmail.com>
2026-01-11 22:35:34 +0100

    We're so back

--- .recover/recover.c
+++ .recover/recover.c
@@ -23,59 +23,71 @@ int main(int argc, char *argv[])
 
     // loop through the contents of the card in 512 size chunk
     uint8_t chunk[CHUNK_SIZE];
-    if (chunk == NULL)
-    {
-        perror("Buffer Memory allocation failed");
-        fclose(card);
-        return 2;
-    }
 
     // read each chunk
     int count = 0;
-    while(fread(chunk, 1, CHUNK_SIZE, card) == CHUNK_SIZE)
+
+    // init image pointer outside loop and only asign image during loop
+    FILE *image = NULL;
+
+    // starts reading chunk
+    while (fread(chunk, 1, CHUNK_SIZE, card) == CHUNK_SIZE)
     {
         // if chunk is header then open file
-        if (chunk[0] == 255 &&
-            chunk[1] == 216 &&
-            chunk[2] == 255 &&
-            (chunk[3] & 240) == 224)
+        if (chunk[0] == 0xff && chunk[1] == 0xd8 && chunk[2] == 0xff && (chunk[3] & 0xf0) == 0xe0)
         {
+
+            // if chunk is a header, before we open a new image to write to check if image is null
+            // if not then close it
+            if (image != NULL)
+            {
+                fclose(image);
+            }
+
             // get image file name
             char filename[8];
             sprintf(filename, "%03i.jpg", count);
 
             // open image
-            FILE *image = fopen(filename, "wb");
+            image = fopen(filename, "wb"); // need to close image after we found a new image
             if (image == NULL)
-            {
+            { // if fialed close card
                 perror("Failed to create image");
                 fclose(card);
-                return 3;
+                return 2;
             }
 
             // write to image
             if (fwrite(chunk, 1, CHUNK_SIZE, image) != CHUNK_SIZE)
-            {
+            { // if fialed close card and image
                 perror("Failed to write to image");
-                fclose(card);
                 fclose(image);
-                return 4;
+                fclose(card);
+                return 2;
             }
 
             count++;
         }
-
-
+        else if (image != NULL)
+        { // write to image
+            if (fwrite(chunk, 1, CHUNK_SIZE, image) != CHUNK_SIZE)
+            { // if fialed close card andimage
+                perror("Failed to write to image");
+                fclose(image);
+                fclose(card);
+                return 2;
+            }
+        }
     }
 
+    // close image at the end of writing
+    if (image != NULL)
+    {
+        fclose(image);
+    }
 
-    // if header then then
-        // open a new file
-        // write to it
-        //
-    // if not keep looping until end of file
     // close everything
     fclose(card);
-    fclose(image);
     return 0;
 }
+
